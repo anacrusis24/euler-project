28431793e0788e536adad7942b5e7baae5698c0f
Austin Villegas <ajv131@gmail.com>
2022-05-01 02:22:10 -0700

    Removed numpy dependency

--- unit_test/test_euler.py
+++ unit_test/test_euler.py
@@ -10,7 +10,6 @@ Desc    :  The unit tests for Euler problems
 
 ##### Imports #####
 import pytest
-import numpy as np
 import os
 import sys
 
@@ -22,6 +21,5 @@ from euler_problem.euler_problem_1_10 import *
 
 ##### Unit Tests #####
 def test_euler_1():
-    sum_mult_3_5 = sum_multiples(np.array([3, 5]), 10)
+    sum_mult_3_5 = sum_multiples([3, 5], 10)
     assert sum_mult_3_5 == 23
-        
