bee0bd4f03805d048f164e816544add828d38057
Austin Villegas <ajv131@gmail.com>
2022-05-01 01:05:07 -0700

    Adding the first euler problem

---
+++ euler_problem/euler_problem_1_10.py
@@ -0,0 +1,42 @@
+'''
+---------- euler_problem_1_10.py ----------
+Time    :  2022/04/30 20:39:22
+Version :  1.0
+Author  :  Austin Villegas 
+Github  :  https://github.com/anacrusis24
+Contact :  ajv131@gmail.com
+Desc    :  Function to solve Euler problems 1 - 10
+'''
+
+##### Imports #####
+import numpy as np 
+
+##### Define Functions #####
+def sum_multiples(multiples, max_num):
+    '''
+    @sum_multiples
+    Function computes the sum of all the natural numbers which are below max_num and are multiples of a number in the multiples list
+    
+    @Input
+    multiples: a list of natural numbers that are the base multiples 
+    max_num: the max number (not inclusive) to calculate the sum
+    
+    @Output
+    running_sum: the final sum of the multiples
+    '''
+    # initialize the running sum and starting number
+    running_sum = 0
+    n = 1
+
+    # calculate the running sum
+    while n < max_num:
+        for mult in multiples:
+            # if n is a multiple of one of the numbers in multiples
+            if n % mult == 0:
+                running_sum += n
+                break
+
+        n += 1
+    
+    return running_sum
+
